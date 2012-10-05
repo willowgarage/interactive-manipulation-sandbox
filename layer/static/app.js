5fd83a981560e8056ceeca3174848506eb5e7b8f
Tessa Lau <tlau@willowgarage.com>
2012-10-05 17:59:35 -0300

    Upgraded to master version of ember. Moved index.html out of Django into static.

--- layer/static/app.js
+++ layer/static/app.js
@@ -63,55 +63,6 @@ App.Robot.reopenClass({
 		return robot;
 	},
 
-	drawMap : function() {
-		var w = 480,
-		  h = 374,
-		  x = d3.scale.linear().domain([0, w])
-		  y = d3.scale.ordinal().domain([0, h]);
-
-		// x, y, width, height, name, place-id
-		var rooms = [
-		  [310, 338, 29, 34, "Tessa's office", 1],
-		  [280, 348, 28, 23, "Chad's office", 2],
-		  [341, 339, 28, 33, "Brandon's office", 3],
-		  [259, 248, 50, 32, "The Cave", 4],
-		  [307, 149, 42, 97, "The Green Room", 5],
-		//  [279, 50, 181, 78, "The cafeteria", 6],
-		  [187, 235, 70, 75, "The pool room", 7],
-		  [72, 190, 102, 73, "The Cathedral", 8]
-		];
-		/* TODO: get room data from an API
-		var rooms = d3.json("<%= url_for(:controller => :room, :action => :list) %>");
-		alert(rooms);
-		*/
-		  var svg = d3.select("#floorplan-div").append("svg")
-			.attr("width", w)
-			.attr("height", h);
-
-		  svg.append("svg:image")
-			.attr("xlink:href", "/static/willow-floorplan.png")
-			.attr("width", w)
-			.attr("height", h);
-
-		  svg.selectAll(".room")
-			.data(rooms)
-			.enter().append("svg:rect")
-			  .attr("class", "room")
-			  .attr("x", function(d) { return d[0]; })
-			  .attr("y", function(d) { return d[1]; })
-			  .attr("width", function(d) { return d[2]; })
-			  .attr("height", function(d) { return d[3]; })
-			  .on("mouseover", function(d) {
-				d3.select("#placename").text(d[4]);
-				})
-			  .on("mouseout", function() {
-				d3.select("#placename").text("");
-				})
-			  .on("click", function(d) {
-				var url = "/change/me" + "/" + d[5];
-				window.location = url;
-				});
-	}
 });
 
 /* ---------------------------------------------------------------------- */
