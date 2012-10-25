88ba6bdaaf2860b9903ceb07d19a4910b1e41ca9
Kaijen Hsiao <hsiao@willowgarage.com>
2012-10-24 19:09:09 -0700

    added loading sim localization from saved file in config

--- smach_executer/scripts/localization_saver.py
+++ smach_executer/scripts/localization_saver.py
@@ -7,7 +7,7 @@ import tf
 import os, pickle
 from geometry_msgs.msg import PoseWithCovarianceStamped, Point, Quaternion
 
-POSE_FILE = os.path.join(os.environ['HOME'], 'state/initial_pose.pck')
+POSE_FILE_DEFAULT = os.path.join(os.environ['HOME'], 'state/initial_pose.pck')
 
 def pprint_str(l):
     return " ".join(["%.3f"%x for x in l])
@@ -23,7 +23,13 @@ if __name__ == '__main__':
     initial_pose_pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped)
     current_pose_pub = rospy.Publisher('/robot_pose', PoseWithCovarianceStamped)
 
-    #wait for the map frame to exist
+    #path+name of pose file
+    pose_file = rospy.get_param("~pose_file", POSE_FILE_DEFAULT)   
+
+    #should we save the current robot pose to file every 5 seconds?
+    save_pose = rospy.get_param("~save_pose", True)
+
+    #wait for the map frame to exist (which means localization is now running)
     trans = 0
     rot = 0
     while not rospy.is_shutdown():
@@ -36,12 +42,12 @@ if __name__ == '__main__':
             continue
         break
 
-    #if robot is not localized, get the old localized pose from file and broadcast it
+    #if robot is not already localized, get the old localized pose from file and broadcast it
     if not check_localized(trans):
-        rospy.loginfo("localization_saver: loading localization from %s" % POSE_FILE)
+        rospy.loginfo("localization_saver: loading localization from %s" % pose_file)
         try:
-            if os.path.exists(POSE_FILE):
-                infile = open(POSE_FILE, 'r')
+            if os.path.exists(pose_file):
+                infile = open(pose_file, 'r')
                 (trans, rot) = pickle.load(infile)
                 rospy.loginfo("localization_saver: loaded trans: %s, rot: %s"%(pprint_str(trans), pprint_str(rot)))                
                 infile.close()
@@ -60,8 +66,8 @@ if __name__ == '__main__':
             pose_msg.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 
                                         0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 
                                         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-                                        0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 
-                                        0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 
+                                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
                                         0.0, 0.0, 0.0, 0.0, 0.0, 0.1]
             #rospy.loginfo("localization_saver: publishing pose_msg: %s"%str(pose_msg))
             initial_pose_pub.publish(pose_msg)
@@ -72,19 +78,22 @@ if __name__ == '__main__':
 
         try:
             (trans,rot) = listener.lookupTransform('/map', '/base_link', rospy.Time(0))
-            if not check_localized(trans):
-                # The current transform is not localized; ignore it
+
+            # If the current transform is not localized, ignore it
+            if not check_localized(trans):                
                 continue
 
-            #rospy.loginfo("localization_saver: saving trans: %s, rot: %s"%(pprint_str(trans), pprint_str(rot)))
-            try:
-                if not os.path.exists(os.path.dirname(POSE_FILE)):
-                    os.mkdir(os.path.dirname(POSE_FILE))
-                outfile = open(POSE_FILE, 'w')
-                pickle.dump((trans,rot), outfile)
-                outfile.close()
-            except Exception, e:
-                rospy.loginfo("localization_saver: Error saving to file")
+            # Save the current robot pose (if desired)
+            if save_pose:
+                rospy.loginfo("localization_saver: saving trans: %s, rot: %s"%(pprint_str(trans), pprint_str(rot)))
+                try:
+                    if not os.path.exists(os.path.dirname(pose_file)):
+                        os.mkdir(os.path.dirname(pose_file))
+                    outfile = open(pose_file, 'w')
+                    pickle.dump((trans,rot), outfile)
+                    outfile.close()
+                except Exception, e:
+                    rospy.loginfo("localization_saver: Error saving to file")
 
             # Broadcast the current location on /robot_pose
             pose_msg = PoseWithCovarianceStamped()
