62c6872c2811f5bddf2eb180f89aa6e1715cd66a
Tessa Lau <tlau@willowgarage.com>
2012-10-30 10:59:16 -0300

    Fix the admin interface to show descriptive names for things, not "Robot object" "Robot object" "Robot object"

--- layer/world/admin.py
+++ layer/world/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from world.models import Place, Robot
 
-admin.site.register(Place)
-admin.site.register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'pose_x', 'pose_y', 'map_x', 'map_y')
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Robot, RobotAdmin)
