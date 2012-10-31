91bbc856b19698213c5e2dbd76e843042f62f024
Julian Cerruti <jcerruti@willowgarage.com>
2012-10-31 15:21:17 -0300

    Port over custom admin for Robots from layer into layer2

--- layer2/server/world/admin.py
+++ layer2/server/world/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from world.models import Place, Robot, Client
+from world.models import Place, Robot
 
-admin.site.register(Place)
-admin.site.register(Robot)
-admin.site.register(Client)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'pose_x', 'pose_y', 'map_x', 'map_y')
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Robot, RobotAdmin)
