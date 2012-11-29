e50ac37030dbebe7499b966af71702e0f7ceaab3
Jonathan Binney <jbinney@bih.willowgarage.com>
2012-11-28 17:31:45 -0800

    added activity_manager action for listing running activities

--- activity_manager/src/activity_manager/activity_server.py
+++ activity_manager/src/activity_manager/activity_server.py
@@ -2,17 +2,13 @@ import json
 import rospy
 import actionlib
 
-# used to convert between json strings and ROS messages
-from rosbridge_library.internal import message_conversion
-
 from activity_msgs.msg import StartActivityAction, StartActivityResult, StopActivityAction, StopActivityResult, ActivityErrorCode
+from activity_msgs.msg import ListRunningActivitiesAction, ListRunningActivitiesResult
 from activity_manager.threaded_activity_manager import ThreadedActivityManager
-from activity_manager.activity_loader import ActivityLoader
 
 class ActivityServer:
     def __init__(self, base_topic):
         self._activity_manager = ThreadedActivityManager()
-        self._activity_loader = ActivityLoader()
 
         start_activity_topic = '%s/start_activity' % base_topic
         self._start_activity_as = actionlib.SimpleActionServer(
@@ -22,22 +18,19 @@ class ActivityServer:
         self._stop_activity_as = actionlib.SimpleActionServer(
             stop_activity_topic, StopActivityAction, self.stop_activity_cb, False)
 
+        list_activities_topic = '%s/list_running_activities' % base_topic
+        self._list_activities_as = actionlib.SimpleActionServer(
+            list_activities_topic, ListRunningActivitiesAction, self.list_running_activities_cb, False)
+
         self._start_activity_as.start()
         self._stop_activity_as.start()
+        self._list_activities_as.start()
 
     def start_activity_cb(self, goal):
         rospy.loginfo('Starting activity: %s' % goal.activity_type)
 
-        # find the python class that implements this activity
-        activity_class = self._activity_loader.get_activity_class(goal.activity_type)
-        activity_goal_class = self._activity_loader.get_activity_goal_class(goal.activity_type)
-
-        # convert the goal from JSON to a ROS message
-        activity_goal_dict = json.loads(goal.goal)
-        activity_goal = message_conversion.populate_instance(activity_goal_dict, activity_goal_class)
-
         # ask the activity manager to start the activity
-        activity_id = self._activity_manager.start_activity(activity_class, activity_goal, goal.desired_activity_id)
+        activity_id = self._activity_manager.start_activity(goal.activity_type, goal.goal, goal.desired_activity_id)
 
         # started successfully (even though the activity itself may end up failing)
         result = StartActivityResult()
@@ -57,7 +50,14 @@ class ActivityServer:
         self._stop_activity_as.set_succeeded(result)
         return
 
-        
+    def list_running_activities_cb(self, goal):
+        rospy.loginfo('Listing running activities')
+
+        result = ListRunningActivitiesResult()
+        result.running_activities = self._activity_manager.get_running_activities()
+        self._list_activities_as.set_succeeded(result)
+        return
+
     def run(self):
         while not rospy.is_shutdown():
             rospy.sleep(0.1)
