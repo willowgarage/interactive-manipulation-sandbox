cba9d5330cab86790eedb37473a6d0648fa9ddd1
Jonathan Binney <jbinney@bih.willowgarage.com>
2012-12-11 21:24:26 -0800

    implemented basic multi-ros functionality in mros

--- mros/src/mros/mros_parent.py
+++ mros/src/mros/mros_parent.py
@@ -1,143 +1,157 @@
-class MRosParent:
-    def __init__(self, child_config_uri, child_sub_uri, child_pub_rui, prefix='', poll_rate=1.):
-        '''
-        Args:
+import threading
+import cPickle as pickle
+import zmq
+import rospy, roslib
 
-        child_config_uri (str) - uri at which child will answer config requests.
-        child_sub_uri (str) - uri at which child will subscribe to messages.
-        child_pub_uri (str) - uri at which child will publish messages.
-        '''
-        self._topics = {}
-        self._topics_lock = threading.Lock()
-        self._prefix = prefix
+from mros.ros_interface import RosInterface
+
+class MRosParent:
+    def __init__(self, ros_master_uri, config_uri, pub_uri, sub_uri, config_dict, prefix='', poll_rate=1.):
+        self._ros_master_uri = ros_master_uri
+        self._config_uri = config_uri
+        self._pub_uri = pub_uri
+        self._sub_uri = sub_uri
+        self._config_dict = config_dict
+        self._prefix = config_dict['prefix']
         self._poll_rate = poll_rate
 
-        # create class for interfacing with the ROS master
-        caller_id = rospy.get_caller_id()
-        self._master = rosgraph.Master(caller_id)
-
+    def initialize(self):
+        # socket for configuration requests
         self._zmq_context = zmq.Context()
-
-        # socket for setting config on child
         self._zmq_config_socket = self._zmq_context.socket(zmq.REQ)
-        self._zmq_config_socket.connect(child_config_socket_uri)
+        self._zmq_config_socket.connect(self._config_uri)
 
-        # socket for sending ROS messages to child
-        self._zmq_pub_socket = self._zmq_context.socket(zmq.PUB)
-        self._zmq_pub_socket.connect(child_sub_socket_uri)
+        # socket for receiving messages
+        rospy.loginfo('Parent listening for messages on zmq socket %s' % self._pub_uri)
+        self._zmq_pub_socket = self._zmq_context.socket(zmq.SUB)
+        self._zmq_pub_socket.connect(self._pub_uri)
+        self._zmq_pub_socket.setsockopt(zmq.SUBSCRIBE, '')
+        self._pub_socket_lock = threading.Lock()
 
-        # socket for receiving ROS messages from child
-        self._zmq_sub_socket = self._zmq_context.socket(zmq.SUB)
-        self._zmq_sub_socket.connect(child_pub_socket_uri)
+        # socket for publishing messages
+        rospy.loginfo('Parent publishing messages on zmq socket %s' % self._sub_uri)
+        self._zmq_sub_socket = self._zmq_context.socket(zmq.PUB)
+        self._zmq_sub_socket.connect(self._sub_uri)
+        self._sub_socket_lock = threading.Lock()
+
+        # used to inspect the local ROS system
+        self._ros_interface = RosInterface(self._ros_master_uri, 'mros_parent', self.ros_message_callback)
+        self._ros_interface_lock = threading.Lock()
+
+        # start thread that publishes ROS messages we get from remote system
+        #publish_thread = threading.Thread(target=self.publish_thread_func)
+        #publish_thread.start()
+
+    def parent_to_child_topic(self, topic):
+        return topic[len(self._prefix):]
+
+    def child_to_parent_topic(self, topic):
+        return self._prefix + topic
 
     def run(self):
-        self._local_update_thread = threading.Thread(target=self.local_update_thread_func)
-        self._remote_update_thread = threading.Thread(target=self.remote_update_thread_func)
-        self._local_update_thread.start()
-        self._remote_update_thread.start()        
-        rospy.spin()
+        self.initialize()
 
-    def received_ros_message_cb(self, msg, topic):
-        # TODO: throttle based on priority
-        self._zmq_data_socket.send((topic, msg))
+        child_topics = []
+        print self._config_dict
+        for child_topic in self._config_dict['topics']:
+            parent_topic = self.child_to_parent_topic(child_topic)
+            
+            topic_dict = self._config_dict['topics'][child_topic]
+            message_type = str(topic_dict['message_type'])
+            message_class = roslib.message.get_message_class(message_type)
+            md5sum = str(message_class._md5sum)
+            child_topics.append((child_topic, message_type, md5sum))
 
-    def local_update(self):
-        '''
-        Checks the ROS master for list of topics, as well as number of
-        subscribers and publishers for each topic.
-        '''
-        # get all topics from the ROS master, and a list of publishers and subscribers for each
-        pubs, subs, services = self._master.getSystemState()
-        pubs = dict(pubs)
-        subs = dict(subs)
-        all_local_topics = set.union(set(pubs.keys()), set(subs.keys()))
+            # subscribe to the topic on the local ROS system
+            rospy.loginfo('Parent subscribing to %s' % parent_topic)
+            self._ros_interface.subscribe(parent_topic, message_type, md5sum)
 
-        # get the types of all topics from the ROS master
-        topic_types = dict(self._master.getTopicTypes())
+            # advertise the topic on the local ROS system
+            rospy.loginfo('Parent advertising %s' % parent_topic)
+            self._ros_interface.advertise(parent_topic, message_type, md5sum)
+
+        # tell the child to advertise each of the forwarded topics
+        command = ['ADVERTISE'] + child_topics
+        self._zmq_config_socket.send(pickle.dumps(command))
+        rep = pickle.loads(self._zmq_config_socket.recv())
+
+        # tell the child to subscribe to each of the forwarded topics
+        command = ['SUBSCRIBE'] + child_topics
+        self._zmq_config_socket.send(pickle.dumps(command))
+        rep = pickle.loads(self._zmq_config_socket.recv())
+
+        while not rospy.is_shutdown():
+            rospy.loginfo('Parent waiting for messages from child')
+            child_topic, msg = pickle.loads(self._zmq_pub_socket.recv())
+            parent_topic = self.child_to_parent_topic(child_topic)
+            rospy.loginfo('Parent publishing message from child topic %s on topic %s' % (child_topic, parent_topic))
+            with self._ros_interface_lock:
+                self._ros_interface.publish(parent_topic, msg)
+
+    def run_dynamic(self):
+        self.initialize()
         
-        with self._topics_lock:
-            # clear previous info on local subscribers/publishers
-            for topic in self._topics:
-                self.num_local_subscribers = 0
-                self.num_local_publishers = 0
+        while not rospy.is_shutdown():
+            rospy.loginfo('MRos parent updating')
+            
+            # get a list of topics from the remote
+            self._zmq_config_socket.send(pickle.dumps(('GET_TOPIC_LIST',)))
+            remote_ros_graph = pickle.loads(self._zmq_config_socket.recv())
 
-            # update info on publishers
-            for topic in all_local_topics:
-                if not topic in self._topics:
-                    self._topics[topic] = TopicInfo(topic)
-                topic_info = self._topics[topic]
+            # inspect the local ros graph
+            self._ros_interface.update_ros_graph()
+            local_ros_graph = self._ros_interface.get_ros_graph()
 
-                # set the topic's message type (this is just a string)
-                topic_info.message_type = topic_types[topic]
-
-                # this is getting the md5sum from the message class, not from the node
-                # publishing the topic. we're assuming that it is using the same message
-                # definition as we find in our ROS_PACKAGE_PATH
-                message_class = roslib.message.get_message_class(topic_info.message_type)
-                if message_class is None:
-                    print 'Warning: no message class for message type %s' % topic_info.message_type
-                    continue
-                topic_info.md5sum = message_class._md5sum
-                    
-                if topic in pubs:
-                    # TODO: remove self from list of publishers                    
-                    topic_info.num_local_publishers = len(pubs[topic])
-
-                if topic in subs:
-                    # TODO: remove self from list of subscribers
-                    topic_info.num_local_subscribers = len(subs[topic])
-
-    def remote_update(self, update_dict):
-        with self._topics_lock:
-            for topic in update_dict:
-                if not topic in self._topics:
-                    self._topics[topic] = TopicInfo(topic)
-
-                topic_info = self._topics[topic]
-                    
-                topic_info.num_remote_subscribers = update_dict[topic]['num_subscribers']
-                topic_info.num_remote_publishers = update_dict[topic]['num_publishers']
-                topic_info.message_type = update_dict[topic]['message_type']
-                topic_info.md5sum = update_dict[topic]['md5sum']
-
+            for topic in remote_ros_graph:
                 # all topics that exist on the remote system should have local publishers, so that
                 # they are visible on the local ROS master
-                if topic_info.publisher is None:
-                    passthrough_message_class = make_passthrough_message_class(topic_info.message_type, topic_info.md5sum)
-                    #topic_info.publisher = rospy.Publisher(topic, passthrough_message_class)
+                rospy.loginfo('Parent advertising %s' % topic)
+                self._ros_interface.advertise(topic, remote_ros_graph[topic]['message_type'], remote_ros_graph[topic]['md5sum'])
 
                 # if there is a subscriber on the remote side, there needs to be a subscriber
                 # on the local side, so that we can forward along all messages on this topic
                 # TODO: destroy local subscriber when there are no more remote subscribers
-                if topic_info.subscriber is None and topic_info.num_remote_subscribers > 0:
-                    passthrough_message_class = make_passthrough_message_class(topic_info.message_type, topic_info.md5sum)
-                    topic_info.subscriber = rospy.Subscriber(topic, passthrough_message_class)
+                if len(remote_ros_graph[topic]['subscribers']) > 0:
+                    rospy.loginfo('Parent subscribing to %s' % topic)
+                    self._ros_interface.subscribe(topic, remote_ros_graph[topic]['message_type'], remote_ros_graph[topic]['md5sum'])
+
+            topics_for_subscription = []
+            topics_for_advertising = []
+            for topic in local_ros_graph:
+                # all topics which are subscribed to locally need to be subscribed to remotely
+                print topic, local_ros_graph[topic]['subscribers']
+                if len(local_ros_graph[topic]['subscribers']) > 0:
+                    # FIXME just for testing
+                    if not topic in ['/test_topic']:
+                        continue
                     
-    def local_update_thread_func(self):
-        poll_rate = rospy.Rate(self._poll_rate)
+                    topics_for_subscription.append((topic, local_ros_graph[topic]['message_type'], local_ros_graph[topic]['md5sum']))
+
+                # all topics which are published locally need to be advertised remotely
+                topics_for_advertising.append((topic, local_ros_graph[topic]['message_type'], local_ros_graph[topic]['md5sum']))
+
+            # send list of topics that the remote side should subscribe to
+            command = ['SUBSCRIBE'] + topics_for_subscription
+            self._zmq_config_socket.send(pickle.dumps(command))
+            rep = pickle.loads(self._zmq_config_socket.recv())
+
+            # send list of topics that the remote side should advertise
+            command = ['ADVERTISE'] + topics_for_advertising
+            self._zmq_config_socket.send(pickle.dumps(command))
+            rep = pickle.loads(self._zmq_config_socket.recv())
+            
+        rospy.spin()
+        
+    def publish_thread_func(self):
         while not rospy.is_shutdown():
-            print 'Running local update'
-            self.local_update()
+            topic, msg = pickle.loads(self._zmq_pub_socket.recv())
+            rospy.loginfo('Parent publishing message on %s' % topic)            
+            with self._ros_interface_lock:
+                self._ros_interface.publish(topic, msg)
 
-            # send an update to the remote
-            update_dict = {}            
-            with self._topics_lock:
-                for topic in self._topics:
-                    topic_info = self._topics[topic]
-                    update_dict[topic] = {
-                        'num_subscribers' : topic_info.num_local_subscribers,
-                        'num_publishers' : topic_info.num_local_publishers,
-                        'md5sum' : topic_info.md5sum,
-                        'message_type' : topic_info.message_type
-                        }
-            self._zmq_config_socket.send(pickle.dumps(update_dict))
-
-            # send the list
-            poll_rate.sleep()
-
-    def remote_update_thread_func(self):
-        while not rospy.is_shutdown():
-            print 'Running remote update'
-            # get a config message from the remote
-            update_dict = pickle.loads(self._zmq_config_socket.recv())
-            self.remote_update(update_dict)
+    def ros_message_callback(self, msg, parent_topic):
+        # TODO: throttle based on priority
+        child_topic = self.parent_to_child_topic(parent_topic)
+        with self._sub_socket_lock:
+            rospy.loginfo('Parent sending ROS message from topic %s to child for topic %s' % (parent_topic, child_topic))
+            self._zmq_sub_socket.send(pickle.dumps((child_topic, msg._buff)))
