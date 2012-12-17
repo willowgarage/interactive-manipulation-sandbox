8dffd1bdd67b7ebd3357d8e7b0d3bdd492dcfc6b
Jonathan Binney <jbinney@bih.willowgarage.com>
2012-12-17 11:26:41 -0800

    halfway through changes to activity_loader

--- activity_manager/src/activity_manager/activity_loader.py
+++ activity_manager/src/activity_manager/activity_loader.py
@@ -1,16 +1,24 @@
 import roslib
-import sys, os.path
+import sys, os.path, inspect
 
 class ActivityLoader:
-    def __init__(self):
+    def __init__(self, packages):
         '''
         Finds and loads python classes which define activities.
         '''
         self._activity_classes = {}
         self._activity_goal_classes = {}
 
+        # hardcoded to look in files in <package_name>/src for subclasses
+        # of activity base
+        for package_name in packages:
+            package_dir = roslib.packages.get_pkg_dir(package_name)
+            sys.path.append(os.path.join(package_dir, 'src'))
+            module = __import__(package_name)
+            
         # TODO hardcoded for the moment; should automatically find classes
         # based on ROS parameters
+
 
         # FIXME Uuuuuugly hack. pr2_activities depends on activity_manager, so activity_manager
         # can't depend on pr2_activities. Instead we manually find pr2_activities and add it to the python
@@ -28,3 +36,6 @@ class ActivityLoader:
 
     def get_activity_goal_class(self, activity_type):
         return self._activity_goal_classes[activity_type]
+
+if __name__ == '__main__':
+    al = ActivityLoader(['pr2_activities'])
