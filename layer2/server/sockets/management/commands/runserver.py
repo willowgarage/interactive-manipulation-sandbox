c3c27760b1f836217f35429e834aeafb0858e3cf
Elvio Toccalino <etoccalino@creativa77.com.ar>
2012-12-18 14:13:20 -0300

    Created middleware to handle servicing socket requests. Reimplemented runserver to use the middleware from new location.

--- layer2/server/sockets/management/commands/runserver.py
+++ layer2/server/sockets/management/commands/runserver.py
@@ -1,60 +1,15 @@
 from gevent import monkey
 monkey.patch_all()
 
-from optparse import make_option
 import re
+from optparse import make_option
+from socketio.server import SocketIOServer
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 
-from socketio import socketio_manage
-from socketio.server import SocketIOServer
-
-# The namespaces served by the socket.io backend.
-# Currently emtpy, due to the fact that gevent-socketio (as socket.io.js)
-# implements heartbeats/reconnect (all that's needed for now).
-namespaces = dict()
-
-
-class WithSocketIO(object):
-    """WSGI middleware to provide differential service to socket requests.
-
-    Requests comming to "socket.io" resource are served to the socketio_manage
-    WSGI application, all others to the Django application.
-    """
-
-    resource = 'socket.io'
-
-    # Note about the resource name:
-    #
-    # Changing the resource name involves changes in three points.
-    #
-    # 1) This middleware check, by way of path.startswith or similar.
-    #
-    # 2) The SocketIOServer instance must be passed the 'namespace' keyword.
-    #
-    # 3) The client should call io.connect(url, {resource: "the-new-name"})
-    #
-    # The concept of "namespace" is unique to gevent-socketio, sometimes
-    # confused to that of "resource" (which is shared with socket.io.js), and
-    # is the result of a rather silly name problem in the python library. For
-    # details about the resource-namespace dichotomy (and other, interesting,
-    # notes), see https://github.com/abourget/gevent-socketio/issues/39.
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        path = environ['PATH_INFO'].strip('/')
-
-        if path.startswith(WithSocketIO.resource):
-            socketio_manage(environ, namespaces)
-        else:
-            return self.app(environ, start_response)
-
-
-###############################################################################
+from sockets.middleware import WithSocketIO
 
 
 naiveip_re = re.compile(r"""^(?:
