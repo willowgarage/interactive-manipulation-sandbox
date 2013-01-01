b834b1a660a587035cf50e8a1c6e590d5f2cfb8b
Kaijen Hsiao <hsiao@willowgarage.com>
2012-12-31 19:43:25 -0800

    added reset_collision_objects to smach_executer actions

---
+++ smach_executer/test/json/execute_reset_all_collision_objs.js
@@ -0,0 +1,11 @@
+{
+    "type": "action",
+    "name": "ResetCollisionObjects",
+    "inputs":
+    {
+        "map": true,
+        "unattached_objects": true,
+        "attached_objects": true,
+        "arm": "both"
+    }
+}
