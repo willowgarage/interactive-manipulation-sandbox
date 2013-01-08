4a2d5c298b7a84280345671b7adf1a8c9bf1cd66
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-08 13:47:15 -0300

    using collect_namespaces() to server socket namespaces.

--- continuousop/django/sockets/views.py
+++ continuousop/django/sockets/views.py
@@ -2,10 +2,12 @@
 This django view is only used to link request to gevent-socketio namespaces
 '''
 from socketio import socketio_manage
-from namespace import ClientNamespace
+from sockets import collect_namespaces
+
+
+namespaces = collect_namespaces()
 
 
 def socketio(request):
-        socketio_manage(request.environ, {
-                        '/client': ClientNamespace,
-                        }, request)
+        # Request which connects the socket.
+        socketio_manage(request.environ, namespaces, request)
