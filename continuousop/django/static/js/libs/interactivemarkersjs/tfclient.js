c82df675e53b5458d405dab52dfe1640218ecc3b
David Gossow <dgossow@willowgarage.com>
2013-01-09 13:34:10 -0800

    updated to most recent interactive marker version

--- continuousop/django/static/js/libs/interactivemarkersjs/tfclient.js
+++ continuousop/django/static/js/libs/interactivemarkersjs/tfclient.js
@@ -12,96 +12,106 @@
     this.fixedFrame = options.fixedFrame || 'base_link';
     this.angularThres = options.angularThres || 2.0;
     this.transThres = options.transThres || 0.01;
+    this.rate = options.rate || 10.0;
     this.goalUpdateDelay = options.goalUpdateDelay !== undefined ? options.goalUpdateDelay : 50;
-        
+
     var options = {
       ros: this.ros,
       serverName: options.serverName || "/tf2_web_republisher",
       actionName: "tf2_web_republisher/TFSubscriptionAction"
     };
-    
+
     this.actionClient = new ActionClient( options );
     this.currentGoal = false;
-    this.frame_cbs = {};
+    this.frame_infos = {};
     this.goalUpdateRequested = false;
   };
 
   TfClient.prototype.__proto__ = EventEmitter2.prototype;
-  
+
   TfClient.prototype.processFeedback = function(tfMsg) {
     var that = this;
     tfMsg.transforms.forEach( function(transform) {
       var frameId = transform.child_frame_id;
-      var cbs = that.frame_cbs[frameId];
-      if ( cbs != undefined ) {
-        cbs.forEach(function(cb) {
-          var t = new Transform(transform.transform.translation,transform.transform.rotation);
-          cb(t);
+      var info = that.frame_infos[frameId];
+      if ( info != undefined ) {
+        info.transform = new Transform(transform.transform.translation,transform.transform.rotation);
+        info.cbs.forEach(function(cb) {
+          cb(info.transform);
         });
       }
     });
   }
-  
+
   TfClient.prototype.requestGoalUpdate = function() {
     if ( !this.goalUpdateRequested ) {
-      console.log("requesting goal update");
       setTimeout(this.updateGoal.bind(this), this.goalUpdateDelay);
       this.goalUpdateRequested = true;
       return;
     }
-    console.log("goal update already requested.");
   }
-  
+
   TfClient.prototype.updateGoal = function() {
-    // Anytime the list of frames changes, 
-    // we will need to send a new goal. 
+    // Anytime the list of frames changes,
+    // we will need to send a new goal.
     if ( this.currentGoal ) {
       this.currentGoal.cancel();
     }
-    
+
     var goalMsg = {
       source_frames: [],
        target_frame: this.fixedFrame,
        angular_thres: this.angularThres,
-       trans_thres: this.transThres     
+       trans_thres: this.transThres,
+       rate: this.rate
     };
-    
+
     var source_frames = [];
-    for (frame in this.frame_cbs ) {
+    for (frame in this.frame_infos ) {
       goalMsg.source_frames.push(frame);
     };
-    
+
     this.currentGoal = new this.actionClient.Goal(goalMsg);
     this.currentGoal.on('feedback', this.processFeedback.bind(this));
     this.currentGoal.send();
-    this.goalUpdateRequested = false;    
+    this.goalUpdateRequested = false;
   }
 
   TfClient.prototype.subscribe = function(frameId,callback) {
+    // make sure the frame id is relative
+    if ( frameId[0] === "/" ) {
+      frameId = frameId.substring(1);
+    }
     // if there is no callback registered for the given frame,
     // create emtpy callback list
-    if ( this.frame_cbs[frameId] == undefined ) {
-      this.frame_cbs[frameId] = [];
+    if ( this.frame_infos[frameId] == undefined ) {
+      this.frame_infos[frameId] = {
+        cbs: [] };
       this.requestGoalUpdate();
+    } else {
+      // if we already have a transform, call back immediately
+      if ( this.frame_infos[frameId].transform != undefined ) {
+        callback( this.frame_infos[frameId].transform );
+      }
     }
-    this.frame_cbs[frameId].push( callback );
+    this.frame_infos[frameId].cbs.push( callback );
   };
-  
+
   TfClient.prototype.unsubscribe = function(frameId,callback) {
-    var cbs = this.frame_cbs[frameId];
-    if ( cbs != undefined ) {
-      var cbIndex = cbs.indexOf( callback );
+    var info = this.frame_infos[frameId];
+    if ( info != undefined ) {
+      var cbIndex = info.cbs.indexOf( callback );
       if ( cbIndex >= 0 ) {
-        cbs.splice(cbIndex, 1);
-        if ( cbs.length == 0 ) {
-          delete this.frame_cbs[frameId];
+        info.cbs.splice(cbIndex, 1);
+        if ( info.cbs.length == 0 ) {
+          delete this.frame_infos[frameId];
         }
-      this.needUpdate = true;      
+      this.needUpdate = true;
       }
     }
   }
 
-  
+
   var Pose = TfClient.Pose = function( position, orientation ) {
     this.position = new Vector3;
     this.orientation = new Quaternion;
@@ -112,7 +122,7 @@
       this.orientation.copy( orientation );
     }
   };
-  
+
   Pose.prototype = {
     constructor: Pose,
     copy: function( pose ) {
@@ -120,7 +130,7 @@
       this.orientation.copy( pose.orientation );
     }
   }
-  
+
   var Transform = TfClient.Transform = function( translation, rotation ) {
     this.translation = new Vector3;
     this.rotation = new Quaternion;
@@ -131,7 +141,7 @@
       this.rotation.copy( rotation );
     }
   };
-  
+
   Transform.prototype = {
     constructor: Transform,
     apply: function( pose ) {
@@ -152,16 +162,16 @@
       this.rotation.copy( transform.rotation );
     }
   }
-  
+
   var Quaternion = TfClient.Quaternion = function( x, y, z, w ) {
     this.x = x || 0;
     this.y = y || 0;
     this.z = z || 0;
     this.w = ( w !== undefined ) ? w : 1;
   };
-  
-  Quaternion.prototype = {  
-    constructor: Quaternion,  
+
+  Quaternion.prototype = {
+    constructor: Quaternion,
     copy: function ( q ) {
       this.x = q.x;
       this.y = q.y;
@@ -172,13 +182,13 @@
     inverse: function () {
       this.conjugate().normalize();
       return this;
-    },  
+    },
     conjugate: function () {
       this.x *= -1;
       this.y *= -1;
       this.z *= -1;
       return this;
-    },  
+    },
     normalize: function () {
       var l = Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w );
       if ( l === 0 ) {
@@ -194,7 +204,7 @@
         this.w = this.w * l;
       }
       return this;
-    },  
+    },
     multiply: function ( a, b ) {
       var qax = a.x, qay = a.y, qaz = a.z, qaw = a.w,
       qbx = b.x, qby = b.y, qbz = b.z, qbw = b.w;
@@ -216,18 +226,18 @@
       dest.y = iy * qw + iw * -qy + iz * -qx - ix * -qz;
       dest.z = iz * qw + iw * -qz + ix * -qy - iy * -qx;
       return dest;
-    },  
+    },
     clone: function () {
       return new Quaternion( this.x, this.y, this.z, this.w );
-    }  
+    }
   }
 
   var Vector3 = TfClient.Vector3 = function ( x, y, z ) {
     this.x = x || 0;
     this.y = y || 0;
-    this.z = z || 0;  
-  };  
-  
+    this.z = z || 0;
+  };
+
   Vector3.prototype = {
     constructor: Vector3,
     copy: function ( v ) {
@@ -235,22 +245,22 @@
       this.y = v.y;
       this.z = v.z;
       return this;
-    },  
+    },
     add: function ( a, b ) {
       this.x = a.x + b.x;
       this.y = a.y + b.y;
       this.z = a.z + b.z;
       return this;
-    },  
+    },
     sub: function ( a, b ) {
       this.x = a.x - b.x;
       this.y = a.y - b.y;
       this.z = a.z - b.z;
       return this;
-    },  
+    },
     clone: function () {
       return new Vector3( this.x, this.y, this.z );
-    }  
+    }
   };
 
   return TfClient;
