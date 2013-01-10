d764b5eb1ea57a66a37e877b2723b4c760a013bd
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-10 09:39:44 -0300

    ObjectController passes the move methods calls through to the model.

--- continuousop/django/static/js/controllers/move.js
+++ continuousop/django/static/js/controllers/move.js
@@ -5,20 +5,6 @@ define([
 ], function(Ember, App, $) {
 
   App.MoveController = Ember.ObjectController.extend({
-    moveForward: function() {
-      this.get('content').moveForward();
-    },
-    moveBack: function() {
-      this.get('content').moveBack();
-    },
-    turnLeft: function() {
-      this.get('content').turnLeft();
-    },
-    turnRight: function() {
-      this.get('content').turnRight();
-    }
-
   });
 
 });
-
