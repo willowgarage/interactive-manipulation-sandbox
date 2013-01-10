3090b4161e6f2169907cbc8678c02fcca0c97389
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-10 17:37:18 -0300

    created ros taking code in world/robot.py and rewrote the namespace.py to use it.

--- continuousop/django/world/namespace.py
+++ continuousop/django/world/namespace.py
@@ -1,33 +1,9 @@
-import copy
-import logging
-
 from socketio.namespace import BaseNamespace
 from sockets.health_monitor import HealthMonitorMixin
+from robot import RobotProxy
 
-
+import logging
 logger = logging.getLogger('robot')
-
-
-class Robot:
-    """
-    """
-    def __init__(self):
-        self.moving = False
-
-    def move_as(self, action):
-        """Sends command to the robot to move using "action"."""
-        self.moving = True
-        logger.debug("moving.... %s" % action)
-
-    def stop_moving(self):
-        """Sends command to robot to stop moving."""
-        logger.debug("...stopping the robot...")
-        self.stopped_moving()
-
-    def stopped_moving(self):
-        """Sends notice to the client that the robot is done."""
-        self.moving = False
-        logger.debug("...done moving.")
 
 
 class Namespace(BaseNamespace, HealthMonitorMixin):
@@ -35,46 +11,35 @@ class Namespace(BaseNamespace, HealthMonitorMixin):
     # The namespace to expose to socket.io client code.
     namespace = '/robot'
 
-    # Template action data for a NavigateToPose action.
-    action = {
-        'name': 'NavigateToPose',
-        'inputs': {
-            'x': 0.0,
-            'y': 0.0,
-            'theta': 0.0,
-            'collision_aware': False,
-            'frame_id': '/base_footprint',
-        },
-    }
-
     def initialize(self):
-        self.robot = Robot()
+        self.robot = RobotProxy()
 
     def recv_disconnect(self):
         if self.robot.moving:
             self.robot.stop_moving()
 
+    def robot_stopped_moving(self):
+        """Inform the client the robot has stoped moving."""
+        self.emit('robot stopped')
+
     # The following five events correspond to the NavigateToPose actions.
 
     def on_move_forward(self):
-        action = copy.deepcopy(self.action)
-        action['inputs']['x'] = 0.2
-        self.robot.move_as(action)
+        logger.debug('NAMESPACE: moving the robot forward.')
+        self.robot.move('move forward')
 
     def on_move_back(self):
-        action = copy.deepcopy(self.action)
-        action['inputs']['x'] = -0.2
-        self.robot.move_as(action)
+        logger.debug('NAMESPACE: moving the robot backwards.')
+        self.robot.move('move back')
 
     def on_turn_left(self):
-        action = copy.deepcopy(self.action)
-        action['inputs']['theta'] = 0.2
-        self.robot.move_as(action)
+        logger.debug('NAMESPACE: turning the robot left.')
+        self.robot.move('turn left')
 
     def on_turn_right(self):
-        action = copy.deepcopy(self.action)
-        action['inputs']['theta'] = -0.2
-        self.robot.move_as(action)
+        logger.debug('NAMESPACE: turning the robot right.')
+        self.robot.move('turn right')
 
     def on_stop_moving(self):
-        self.robot.stop_moving()
+        logger.debug('NAMESPACE: stopping the robot.')
+        self.robot.stop_moving(self.robot_stopped_moving)
