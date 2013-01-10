5194f0cbb49c5c533cd6a6cab57073e1eba8e5f9
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-10 10:36:25 -0300

    added logging and a wrapper to the robot socket.io server namespace.

--- continuousop/django/world/namespace.py
+++ continuousop/django/world/namespace.py
@@ -1,7 +1,33 @@
 import copy
+import logging
 
 from socketio.namespace import BaseNamespace
 from sockets.health_monitor import HealthMonitorMixin
+
+
+logger = logging.getLogger('robot')
+
+
+class Robot:
+    """
+    """
+    def __init__(self):
+        self.moving = False
+
+    def move_as(self, action):
+        """Sends command to the robot to move using "action"."""
+        self.moving = True
+        logger.debug("moving.... %s" % action)
+
+    def stop_moving(self):
+        """Sends command to robot to stop moving."""
+        logger.debug("...stopping the robot...")
+        self.stopped_moving()
+
+    def stopped_moving(self):
+        """Sends notice to the client that the robot is done."""
+        self.moving = False
+        logger.debug("...done moving.")
 
 
 class Namespace(BaseNamespace, HealthMonitorMixin):
@@ -21,27 +47,34 @@ class Namespace(BaseNamespace, HealthMonitorMixin):
         },
     }
 
-    # The following four events correspond to the NavigateToPose actions.
+    def initialize(self):
+        self.robot = Robot()
+
+    def recv_disconnect(self):
+        if self.robot.moving:
+            self.robot.stop_moving()
+
+    # The following five events correspond to the NavigateToPose actions.
 
     def on_move_forward(self):
         action = copy.deepcopy(self.action)
         action['inputs']['x'] = 0.2
-        print "COMMAND CAPTURED: move forward"  # DEBUG
+        self.robot.move_as(action)
 
     def on_move_back(self):
         action = copy.deepcopy(self.action)
         action['inputs']['x'] = -0.2
-        print "COMMAND CAPTURED: move backward"  # DEBUG
+        self.robot.move_as(action)
 
     def on_turn_left(self):
         action = copy.deepcopy(self.action)
         action['inputs']['theta'] = 0.2
-        print "COMMAND CAPTURED: turn left"  # DEBUG
+        self.robot.move_as(action)
 
     def on_turn_right(self):
         action = copy.deepcopy(self.action)
         action['inputs']['theta'] = -0.2
-        print "COMMAND CAPTURED: turn right"  # DEBUG
+        self.robot.move_as(action)
 
     def on_stop_moving(self):
-        print "COMMAND CAPTURED: stop moving"  # DEBUG
+        self.robot.stop_moving()
