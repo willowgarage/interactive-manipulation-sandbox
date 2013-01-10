9b6e74585029957fa34ba04ff785247ed3fd8830
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-01-10 09:38:50 -0300

    using didInsertElement to associate behaviour js to the move-base buttons.

--- continuousop/django/static/js/views/move.js
+++ continuousop/django/static/js/views/move.js
@@ -4,9 +4,37 @@ define([
   'text!templates/move.handlebars'
 ], function( Ember, App, moveHtml) {
 
-    App.MoveView = Ember.View.extend({
-      template: Ember.Handlebars.compile(moveHtml)
+    var registerButton = function ($button, robot, startMovingCallback, stopMovingCallback) {
+      $button.on('mousedown', function (event) {
+        event.preventDefault();
+        startMovingCallback.apply(robot);;
+        $button.on('mouseup mouseout', function (event) {
+          stopMovingCallback.apply(robot);
+          $button.off('mouseup mouseout');
+        });
+      });
+    };
 
+    App.MoveView = Ember.View.extend({
+      template: Ember.Handlebars.compile(moveHtml),
+
+      didInsertElement: function () {
+        var controller = this.get('controller')
+          , robot = this.get('context')
+          , bindButton = function (selector, motionName) {
+              return registerButton(
+                Ember.$(selector),
+                robot,
+                controller.get(motionName),
+                controller.get('stopMoving')
+              );
+          };
+
+        bindButton("#move-forward", "moveForward");
+        bindButton("#move-back", 'moveBack');
+        bindButton("#turn-left", 'turnLeft');
+        bindButton("#turn-right", 'turnRight');
+      }
     });
 
 });
