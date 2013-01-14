e801264f640cc4d17ad2cb6ce3158fed2eb947e9
Jonathan Binney <jbinney@bih.willowgarage.com>
2013-01-14 12:52:44 -0800

    removed activity_manager

--- activity_manager/src/activity_manager/activity_base.py
+++
@@ -1,8 +0,0 @@
-import threading
-
-class ActivityBase(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        raise NotImplementedError("Activity's run routine is not defined")
