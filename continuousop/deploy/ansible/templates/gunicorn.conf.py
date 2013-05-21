777cfb1f78a729c2fb0267add9e75d0fa73759dc
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-05-21 17:17:03 -0300

    tune down gunicorn workers to match previous deployment

--- continuousop/deploy/ansible/templates/gunicorn.conf.py
+++ continuousop/deploy/ansible/templates/gunicorn.conf.py
@@ -2,4 +2,4 @@ bind = '127.0.0.1:{{ gunicorn_port }}'
 logfile = '{{ webapps_dir }}/{{ app_name }}/log/gunicorn.log'
 
 worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
-workers = 4
+workers = 1
