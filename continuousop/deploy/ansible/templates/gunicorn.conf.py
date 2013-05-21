e4defd7b9cea5e1f96eb6ea935a77fd0c2743bf1
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-05-21 17:12:33 -0300

    allow nginx>1.3.13 to proxy socket.io connections to gunicorn.

--- continuousop/deploy/ansible/templates/gunicorn.conf.py
+++ continuousop/deploy/ansible/templates/gunicorn.conf.py
@@ -2,4 +2,4 @@ bind = '127.0.0.1:{{ gunicorn_port }}'
 logfile = '{{ webapps_dir }}/{{ app_name }}/log/gunicorn.log'
 
 worker_class = 'socketio.sgunicorn.GeventSocketIOWorker'
-workers = 1
+workers = 4
